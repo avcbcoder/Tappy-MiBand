6395be090626ad7bb47159851cb8f142a771a6df
Ankit Verma <ankitvermamat@gmail.com>
2018-11-21 01:30:43 +0530

    added TIMER class in SETTINGS

--- Wind Up/MainScreen/app/src/main/java/com/av/mainscreen/activity/TimerSettingActivity.java
+++ Wind Up/MainScreen/app/src/main/java/com/av/mainscreen/activity/TimerSettingActivity.java
@@ -2,9 +2,22 @@ package com.av.mainscreen.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
 import com.av.mainscreen.R;
+import com.av.mainscreen.constants.SETTINGS;
+import com.rm.rmswitch.RMSwitch;
 
 public class TimerSettingActivity extends AppCompatActivity {
+
+    ImageButton btn_back;
+    RMSwitch tog_showText, tog_vibrateAtInterval;
+    Spinner spinner_stopDelay, spinner_stopRepeat, spinner_intervalDelay, spinner_intervalRepeat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Wind Up/MainScreen/app/src/main/java/com/av/mainscreen/constants/SETTINGS.java
+++ Wind Up/MainScreen/app/src/main/java/com/av/mainscreen/constants/SETTINGS.java
@@ -38,18 +38,18 @@ public class SETTINGS {
             DELAY_BTW_MULTIPLE_COMMANDS = new Integer(arr[5]);
         }
 
-        public static int getPosClickInterval(){
-            if(CLICK_INTERVAL==600)
+        public static int getPosClickInterval() {
+            if (CLICK_INTERVAL == 600)
                 return 0;
-            else if(CLICK_INTERVAL==1700)
+            else if (CLICK_INTERVAL == 1700)
                 return 2;
             return 1;
         }
 
-        public static int getPosDelayMutiple(){
-            if(DELAY_BTW_MULTIPLE_COMMANDS==500)
+        public static int getPosDelayMutiple() {
+            if (DELAY_BTW_MULTIPLE_COMMANDS == 500)
                 return 0;
-            else if(DELAY_BTW_MULTIPLE_COMMANDS==1500)
+            else if (DELAY_BTW_MULTIPLE_COMMANDS == 1500)
                 return 2;
             return 1;
         }
@@ -79,6 +79,7 @@ public class SETTINGS {
     public static final String DEF_THREE_TAP = "0 3 1 300 1 1";
     public static final String DEF_COMMON_SETTING = "1 1 0 1 1150 1000";
     public static final String DEF_CALL_SETTING = "0 (Can't talk to you right now! CALL me later?)";
+    public static final String DEF_TIMER = "1 1 300 2 300 1";
 
     public static TAP[] taps = {null, new TAP(DEF_ONE_TAP), new TAP(DEF_TWO_TAP), new TAP(DEF_THREE_TAP)};
 
@@ -111,16 +112,35 @@ public class SETTINGS {
             return VIBRATE_DELAY / 100 - 1; /* I ended up wasting 20 minutes bexause of using % instead of /*/
         }
 
-        public int getPosRepeat(){
-            return VIBRATE_REPEAT-1;
+        public int getPosRepeat() {
+            return VIBRATE_REPEAT - 1;
         }
     }
 
     public static class TIMER {
-        public static boolean INTERMEDIATE_VIBRATION;
-        public static int TIME;// in milliseconds
-        public static int VIBRATION_AT_END;// -1 if doesn't
-        public static int MESSAGE_AT_END;
-        public static int MESSAGE_AT_START;
+        public static boolean VIBRATE_AT_INTERVAL;
+        public static boolean SHOW_TEXT;
+        public static int VIBRATION_STOP_DELAY;
+        public static int VIBRATION_STOP_REPEAT;
+        public static int VIBRATION_INTERVAL_DELAY;
+        public static int VIBRATION_INTERVAL_REPEAT;
+        public static String MESSAGE_AT_END = "Timer stop";
+        public static String MESSAGE_AT_START = "TIMER started";
+
+        public static String getStringform() {
+            return String.format("%d %d %d %d %d %d", VIBRATE_AT_INTERVAL ? 1 : 0, SHOW_TEXT ? 1 : 0,
+                    VIBRATION_STOP_DELAY, VIBRATION_STOP_REPEAT,
+                    VIBRATION_INTERVAL_DELAY, VIBRATION_INTERVAL_REPEAT);
+        }
+
+        public static void setFromString(String s){
+            String []arr=s.split(" ");
+            VIBRATE_AT_INTERVAL=new Integer(arr[0])==1;
+            SHOW_TEXT=new Integer(arr[1])==1;
+            VIBRATION_STOP_DELAY=new Integer(arr[2]);
+            VIBRATION_STOP_REPEAT=new Integer(arr[3]);
+            VIBRATION_INTERVAL_DELAY=new Integer(arr[4]);
+            VIBRATION_INTERVAL_REPEAT=new Integer(arr[5]);
+        }
     }
 }
