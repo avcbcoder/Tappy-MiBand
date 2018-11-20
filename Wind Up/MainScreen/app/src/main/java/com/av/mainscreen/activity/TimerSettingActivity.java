b6f0b98e40b877945257bbdb85c82464c3e0809f
Ankit Verma <ankitvermamat@gmail.com>
2018-11-20 23:58:22 +0530

    updated timer XML

---
+++ Wind Up/MainScreen/app/src/main/java/com/av/mainscreen/activity/TimerSettingActivity.java
@@ -0,0 +1,14 @@
+package com.av.mainscreen.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.av.mainscreen.R;
+
+public class TimerSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_setting);
+    }
+}
